2bfcd1611514604e77223a7f631365f7d0cb29a9
Mats Otten <mats@kepsi.nl>
2015-03-25 23:17:37 +0100

    better reading

--- src/serial/port/controller/SerialPortControl.java
+++ src/serial/port/controller/SerialPortControl.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 import javax.comm.*;
 
-public class SerialPortControl implements Runnable, SerialPortEventListener {
+public class SerialPortControl implements Runnable {
 	private static final String PORT_ID = "COM3"; // Linux: /dev/term/a
 
 	static CommPortIdentifier portId;
@@ -36,24 +36,52 @@ public class SerialPortControl implements Runnable, SerialPortEventListener {
 		catch (PortInUseException e) {
 			System.out.println(e);
 		}
+
+
 		try {
 			inputStream = serialPort.getInputStream();
 		}
 		catch (IOException e) {
 			System.out.println(e);
 		}
+
+
 		try {
-			serialPort.addEventListener(this);
+			serialPort.addEventListener(new SerialPortEventListener() {
+				public void serialEvent(SerialPortEvent event) {
+					if(event.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+
+						byte[] readBuffer = new byte[20];
+
+						try {
+							while (inputStream.available() > 0) {
+								int numBytes = inputStream.read(readBuffer);
+							}
+							System.out.print(new String(readBuffer));
+						}
+						catch (IOException e) {
+							System.out.println(e);
+						}
+
+					}
+				}
+			});
 		} catch (TooManyListenersException e) {
 			System.out.println(e);
 		}
+
+
 		serialPort.notifyOnDataAvailable(true);
+
+
 		try {
-			serialPort.setSerialPortParams(9600,
-					SerialPort.DATABITS_8,
-					SerialPort.STOPBITS_1,
-					SerialPort.PARITY_NONE);
-		} catch (UnsupportedCommOperationException e) {System.out.println(e);}
+			serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+		}
+		catch (UnsupportedCommOperationException e) {
+			System.out.println(e);
+		}
+
+
 		readThread = new Thread(this);
 		readThread.start();
 	}
@@ -62,31 +90,6 @@ public class SerialPortControl implements Runnable, SerialPortEventListener {
 		try {
 			Thread.sleep(20000);
 		} catch (InterruptedException e) {System.out.println(e);}
-	}
-
-	public void serialEvent(SerialPortEvent event) {
-		switch(event.getEventType()) {
-			case SerialPortEvent.BI:
-			case SerialPortEvent.OE:
-			case SerialPortEvent.FE:
-			case SerialPortEvent.PE:
-			case SerialPortEvent.CD:
-			case SerialPortEvent.CTS:
-			case SerialPortEvent.DSR:
-			case SerialPortEvent.RI:
-			case SerialPortEvent.OUTPUT_BUFFER_EMPTY:
-				break;
-			case SerialPortEvent.DATA_AVAILABLE:
-				byte[] readBuffer = new byte[20];
-
-				try {
-					while (inputStream.available() > 0) {
-						int numBytes = inputStream.read(readBuffer);
-					}
-					System.out.print(new String(readBuffer));
-				} catch (IOException e) {System.out.println(e);}
-				break;
-		}
 	}
 
 
